96a136c837f3c1d46a0e4755f6c7ad64fb2d75ec
zhaojie <961819766@qq.com>
2019-03-08 10:36:05 +0800

    按照ID进行精确查询

--- GraphDemo.Movies/Schema/OrdersQuery.cs
+++ GraphDemo.Movies/Schema/OrdersQuery.cs
@@ -1,8 +1,5 @@
 ﻿using GraphDemo.Orders.Services;
 using GraphQL.Types;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace GraphDemo.Orders.Schema
 {
@@ -13,6 +10,18 @@ namespace GraphDemo.Orders.Schema
             Name = "Query";
 
             Field<ListGraphType<OrderType>>("orders", resolve: context => orderService.GetAsync());
+
+            FieldAsync<OrderType>("getOrderById",
+                arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "id" }),
+                resolve: async context =>
+                 {
+                     var Id = context.GetArgument<int>("id");
+                     var result = await orderService.GetByIdAsync(Id);
+                     return result;
+                 }
+                );
         }
+       
+        
     }
 }
--- GraphDemo.Movies/Services/IOrderService.cs
+++ GraphDemo.Movies/Services/IOrderService.cs
@@ -1,7 +1,5 @@
 ﻿using GraphDemo.Orders.Models;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GraphDemo.Orders.Services
